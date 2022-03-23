faae73bee6914929ae4346f90268f494b623d9bd
Matheus Gomes <gomes.matheusfelipe@gmail.com>
2022-03-22 23:32:31 -0300

    Add done and list descriptions

--- cmd/done.go
+++ cmd/done.go
@@ -40,14 +40,9 @@ func doneRun(cmd *cobra.Command, args []string) {
 // doneCmd represents the done command
 var doneCmd = &cobra.Command{
 	Use:   "done",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: doneRun,
+	Short: "Mark a todo item as done",
+	Long:  `Mark a todo item as done`,
+	Run:   doneRun,
 }
 
 func init() {
--- cmd/list.go
+++ cmd/list.go
@@ -34,14 +34,9 @@ func listRun(cmd *cobra.Command, args []string) {
 // listCmd represents the list command
 var listCmd = &cobra.Command{
 	Use:   "list",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: listRun,
+	Short: "List todo items",
+	Long:  `List todo items`,
+	Run:   listRun,
 }
 
 func init() {
